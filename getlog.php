2f08150b6dc5a9f6784e5fdcd71f5f19d4d03ab0
folbo <jacunia@op.pl>
2015-10-22 01:10:57 +0200

    Fix some nicks aren't parsed correctly.

--- getlog.php
+++ getlog.php
@@ -7,7 +7,9 @@
 	$logfile = fopen($filename, "r") or die("Nie ma takiego logu.");
 	
 	$regex_link = '/(ftp|https?):\/\/(\w+:?\w*@)?(\S+)(:[0-9]+)?(\/([\w#!:.?+=&%@!\/-])?)?/';
-	
+	$regex_nick = '/\<[ \+\@].+\>/';
+	$regex_timestamp = '/[0-9]{2}:[0-9]{2}/';
+
 	while(!feof($logfile)) {
 		$line = fgets($logfile);
 
@@ -17,12 +19,17 @@
 			continue;
 		}
 
+
 		//define header containing timestamp and nick
-		$pos = strpos($line, ">") + 1;
-		$header = explode(" ", substr($line, 0, $pos));
+		preg_match($regex_timestamp, $line, $matches);
+		$timestamp = $matches[0];
+
+		preg_match($regex_nick, $line, $matches, PREG_OFFSET_CAPTURE);
+		$nick = $matches[0][0];
+		$pos_start_content = $matches[0][1] + strlen($nick) + 1;
 
 		//define content message
-		$content = substr($line, $pos + 1);
+		$content = substr($line, $pos_start_content);
 
 		//parse urls
 		preg_match_all($regex_link, $content, $matches, PREG_OFFSET_CAPTURE);
@@ -35,8 +42,8 @@
 		}
 
 		//print output
-		print '<span class="timestamp">' . htmlspecialchars($header[0]) . '</span> ';
-		print '<span class="nick">' . htmlspecialchars($header[1]) . '</span> ';
+		print '<span class="timestamp">' . htmlspecialchars($timestamp) . '</span> ';
+		print '<span class="nick">' . htmlspecialchars($nick) . '</span> ';
 		print $content;
 		print "<br>";
 	}
